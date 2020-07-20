c2cae803af94e3ae62623f87d78a2a56f1b46b6c
richierob62 <richierob62@gmail.com>
2020-07-19 22:56:18 -0400

    Convert LoginMutation to use hooks

--- frontend/generated/apolloComponents.tsx
+++ frontend/generated/apolloComponents.tsx
@@ -1,12 +1,8 @@
-import * as ApolloReactCommon from '@apollo/react-common';
-import * as ApolloReactComponents from '@apollo/react-components';
-import * as ApolloReactHoc from '@apollo/react-hoc';
-import * as React from 'react';
-
 import gql from 'graphql-tag';
+import * as ApolloReactCommon from '@apollo/react-common';
+import * as ApolloReactHooks from '@apollo/react-hooks';
 export type Maybe<T> = T | null;
 export type Exact<T extends { [key: string]: any }> = { [K in keyof T]: T[K] };
-export type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
 /** All built-in and custom scalars, mapped to their actual values */
 export type Scalars = {
   ID: string;
@@ -69,21 +65,26 @@ export type Mutation = {
   resetPassword: Scalars['Boolean'];
 };
 
+
 export type MutationConfirmEmailArgs = {
   token: Scalars['String'];
 };
+
 
 export type MutationForgotPasswordArgs = {
   data: EmailInput;
 };
 
+
 export type MutationLoginArgs = {
   data: LoginInput;
 };
 
+
 export type MutationRegisterArgs = {
   data: RegisterInput;
 };
+
 
 export type MutationResetPasswordArgs = {
   data: PasswordResetInput;
@@ -93,81 +94,49 @@ export type LoginMutationVariables = Exact<{
   data: LoginInput;
 }>;
 
-export type LoginMutation = { __typename?: 'Mutation' } & {
-  login?: Maybe<
-    { __typename?: 'User' } & Pick<
-      User,
-      'id' | 'firstName' | 'lastName' | 'email' | 'confirmed'
-    >
-  >;
-};
 
-export const LoginDocument = gql`
-  mutation Login($data: LoginInput!) {
-    login(data: $data) {
-      id
-      firstName
-      lastName
-      email
-      confirmed
-    }
-  }
-`;
-export type LoginMutationFn = ApolloReactCommon.MutationFunction<
-  LoginMutation,
-  LoginMutationVariables
->;
-export type LoginComponentProps = Omit<
-  ApolloReactComponents.MutationComponentOptions<
-    LoginMutation,
-    LoginMutationVariables
-  >,
-  'mutation'
->;
-
-export const LoginComponent = (props: LoginComponentProps) => (
-  <ApolloReactComponents.Mutation<LoginMutation, LoginMutationVariables>
-    mutation={LoginDocument}
-    {...props}
-  />
+export type LoginMutation = (
+  { __typename?: 'Mutation' }
+  & { login?: Maybe<(
+    { __typename?: 'User' }
+    & Pick<User, 'id' | 'firstName' | 'lastName' | 'email' | 'confirmed'>
+  )> }
 );
 
-export type LoginProps<
-  TChildProps = {},
-  TDataName extends string = 'mutate'
-> = {
-  [key in TDataName]: ApolloReactCommon.MutationFunction<
-    LoginMutation,
-    LoginMutationVariables
-  >;
-} &
-  TChildProps;
-export function withLogin<
-  TProps,
-  TChildProps = {},
-  TDataName extends string = 'mutate'
->(
-  operationOptions?: ApolloReactHoc.OperationOption<
-    TProps,
-    LoginMutation,
-    LoginMutationVariables,
-    LoginProps<TChildProps, TDataName>
-  >
-) {
-  return ApolloReactHoc.withMutation<
-    TProps,
-    LoginMutation,
-    LoginMutationVariables,
-    LoginProps<TChildProps, TDataName>
-  >(LoginDocument, {
-    alias: 'login',
-    ...operationOptions,
-  });
+
+export const LoginDocument = gql`
+    mutation Login($data: LoginInput!) {
+  login(data: $data) {
+    id
+    firstName
+    lastName
+    email
+    confirmed
+  }
 }
-export type LoginMutationResult = ApolloReactCommon.MutationResult<
-  LoginMutation
->;
-export type LoginMutationOptions = ApolloReactCommon.BaseMutationOptions<
-  LoginMutation,
-  LoginMutationVariables
->;
+    `;
+export type LoginMutationFn = ApolloReactCommon.MutationFunction<LoginMutation, LoginMutationVariables>;
+
+/**
+ * __useLoginMutation__
+ *
+ * To run a mutation, you first call `useLoginMutation` within a React component and pass it any options that fit your needs.
+ * When your component renders, `useLoginMutation` returns a tuple that includes:
+ * - A mutate function that you can call at any time to execute the mutation
+ * - An object with fields that represent the current status of the mutation's execution
+ *
+ * @param baseOptions options that will be passed into the mutation, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options-2;
+ *
+ * @example
+ * const [loginMutation, { data, loading, error }] = useLoginMutation({
+ *   variables: {
+ *      data: // value for 'data'
+ *   },
+ * });
+ */
+export function useLoginMutation(baseOptions?: ApolloReactHooks.MutationHookOptions<LoginMutation, LoginMutationVariables>) {
+        return ApolloReactHooks.useMutation<LoginMutation, LoginMutationVariables>(LoginDocument, baseOptions);
+      }
+export type LoginMutationHookResult = ReturnType<typeof useLoginMutation>;
+export type LoginMutationResult = ApolloReactCommon.MutationResult<LoginMutation>;
+export type LoginMutationOptions = ApolloReactCommon.BaseMutationOptions<LoginMutation, LoginMutationVariables>;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -2,11 +2,17 @@ import * as React from 'react';
 
 import Layout from '../components/Layout';
 import Link from 'next/link';
-import { LoginComponent } from '../generated/apolloComponents';
 import { Mutation } from 'react-apollo';
 import { gql } from 'apollo-boost';
+import { useLoginMutation } from '../generated/apolloComponents';
 
 const IndexPage: React.FunctionComponent = () => {
+  const [login, { loading }] = useLoginMutation();
+
+  if (loading) {
+    return <div>Loading</div>;
+  }
+
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       <h1>hello Next.js 👋</h1>
@@ -15,24 +21,21 @@ const IndexPage: React.FunctionComponent = () => {
           <a>About</a>
         </Link>
       </p>
-      <LoginComponent>
-        {(mutate) => (
-          <button
-            onClick={async () => {
-              const response = await mutate({
-                variables: {
-                  data: { email: 'foo11@example.com', password: '1234567' },
-                },
-              });
-              if (response?.data?.login) {
-                console.log(response.data.login);
-              }
-            }}
-          >
-            call login mutation
-          </button>
-        )}
-      </LoginComponent>
+      <button
+        onClick={async () => {
+          const response = await login({
+            variables: {
+              data: { email: 'foo11@example.com', password: '1234567' },
+            },
+          });
+          if (response?.data?.login) {
+            console.log(response.data.login);
+          }
+        }}
+      >
+        call login mutation
+      </button>
+
       <Mutation
         mutation={gql`
           mutation {
