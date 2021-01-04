20fdbb0c7b0a4448c02b085185aa5f5eef82fb2d
richierob62 <richierob62@gmail.com>
2021-01-03 19:58:48 -0500

    add auth middleware

--- backend/modules/posts/DeletePostResolver.ts
+++ backend/modules/posts/DeletePostResolver.ts
@@ -1,20 +1,16 @@
-import { Mutation, Resolver, Arg, UseMiddleware} from 'type-graphql';
-import { Post } from '../../entity/Post'
-import { isPostOwner } from '../../middleware/is_post_owner'
+import { Mutation, Resolver, Arg, UseMiddleware } from 'type-graphql';
+import { Post } from '../../entity/Post';
+import { isAuthenticated } from '../../middleware/is_authenticated';
+import { isPostOwner } from '../../middleware/is_post_owner';
 
 @Resolver()
 export class DeletePostResolver {
+  @UseMiddleware(isAuthenticated)
   @UseMiddleware(isPostOwner)
   @Mutation(() => Boolean)
-  async deletePost(
-    @Arg('id') id : number,
-  ): Promise<boolean> {
+  async deletePost(@Arg('id') id: number): Promise<boolean> {
+    await Post.delete({ id });
 
-    await Post.delete(
-      { id }
-    );
-
-    return true
-
+    return true;
   }
 }
